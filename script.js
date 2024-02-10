1ec3881310a10893e1b881f76b6d399e3570cc46
megler <majeresmom@att.net>
2024-02-10 09:20:46 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+  const NUM_SQUARES = 9;
+  for (i = 0; i < NUM_SQUARES; i++) {
+    id = "sq" + i;
+    document.write("<div class='square' id='" + id + "'></div>");
+  }
+};
