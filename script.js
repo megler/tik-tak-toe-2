004beb54062d9ddc7166b8c79e2d287d41ca9c18
megler <majeresmom@att.net>
2024-02-10 10:02:10 -0500

    ttt3 done.

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 window.onload = function () {
   const NUM_SQUARES = 9;
+
+  let placeMarker = event => {
+    const addX = "X";
+    event.target.innerHTML = addX;
+    alert(event.target.id.slice(2));
+  };
+
   for (i = 0; i < NUM_SQUARES; i++) {
     id = "sq" + i;
-    document.write("<div class='square' id='" + id + "'></div>");
+    let gameSquare = document.createElement("div");
+    gameSquare.id = id;
+    gameSquare.className = "square";
+    document.getElementById("gameboard").appendChild(gameSquare);
+    gameSquare.addEventListener("click", placeMarker);
   }
 };
